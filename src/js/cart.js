50b8035e13f9a552383403df9a7ccd7c2a632e1f
Brandon Smith <smi11037@byui.edu>
2023-01-13 22:37:47 -0700

    Fixed multiple products not showing in cart. Still having an issue getting the image the-north-face-talus-tent... to show. The image will appear if I edit code in browser "<img src="../images/tents/.." but without doing that it defaults to the alt text.

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,24 +2,34 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = [getLocalStorage("so-cart")];
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+  let items = cartItems[0];
+  let markup = "";
+  const htmlItems = items.map((i) => cartItemTemplate(i));
+
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
 function cartItemTemplate(item) {
+  // Separate needed values for testing
+  let obj = JSON.parse(item);
+  let name = obj["Name"];
+  let img = obj["Image"];
+  let color = obj["Colors"];
+  let price = obj["FinalPrice"];
+  // Create html code
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
-      src="${item.Image}"
-      alt="${item.Name}"
+      src="${img}"
+      alt="${name}"
     />
   </a>
   <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
+    <h2 class="card__name">${name}</h2>
   </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__color">${color[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
+  <p class="cart-card__price">$${price}</p>
 </li>`;
 
   return newItem;
--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,21 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get cart from storage
+  let cart = [getLocalStorage("so-cart")];
+  let all = [];
+  // Check if first product
+  if (cart.length == 1 && cart[0] == null) {
+    // If first set product in array
+    cart = [JSON.stringify(product)];
+  } else {
+    cart.push(JSON.stringify(product));
+  }
+
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {

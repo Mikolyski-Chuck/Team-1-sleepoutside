41cf6609a6e2a323f3e7ba143cf066f664dd5d4f
Brandon Smith <smi11037@byui.edu>
2023-01-14 14:21:29 -0700

    Fixed duplicate items showing as multiple entries instead of as one. Now checks for duplicates and records the count as "qty" in display.

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,10 +4,44 @@ function renderCartContents() {
   // Get cart items
   const cartItems = [getLocalStorage("so-cart")];
   let items = cartItems[0];
+
   // Flatten 4 levels (why 4 well why not 4)
   items = items.flat(4);
+  const objArr = items.map(x => JSON.parse(x));
+  let dist = [];
+  // Loop through items to mark duplicates as additional value in qty
+  for (let i in objArr) {
+    // Qty count
+    let count = 0;
+    // Look for duplicate items in array
+    for (let x in objArr) {
+      if (objArr[i]["Name"] == objArr[x]["Name"]) {
+        count++;
+      }
+    }
+
+    // Check if copy of item is already in dist array
+    let first = true;
+    for (let x in dist) {
+      if (dist[x]["Id"] == objArr[i]["Id"]) {
+        first = false
+      }
+    }
+
+    // If copy is not already in array add it with count qty element
+    if (first) {
+      let item = objArr[i];
+      item["qty"] = count;
+      dist.push(item);
+    }
+  }
+
+  // Convert all object back to string
+  const all = dist.map(x => JSON.stringify(x));
+
   // Generate HTML from template for each item
-  const htmlItems = items.map((i) => cartItemTemplate(i));
+  const htmlItems = all.map((i) => cartItemTemplate(i));
+
   // Assign HTML to page
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
@@ -19,6 +53,7 @@ function cartItemTemplate(item) {
   let img = obj["Image"];
   let color = obj["Colors"];
   let price = obj["FinalPrice"];
+  let qty = obj["qty"];
   // Create html code
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
@@ -32,7 +67,7 @@ function cartItemTemplate(item) {
       <h2 class="card__name">${name}</h2>
     </a>
     <p class="cart-card__color">${color[0].ColorName}</p>
-    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__quantity">qty: ${qty}</p>
     <p class="cart-card__price">$${price}</p>
   </div>
 </li>`;

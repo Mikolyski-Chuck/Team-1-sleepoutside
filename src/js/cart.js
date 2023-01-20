fb16b52d6c9aa35a9057b2c9c2421feb97f28098
Brandon Smith <smi11037@byui.edu>
2023-01-19 21:18:46 -0700

    Added a subtotal feature to the cart.

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,22 +1,13 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
-function renderCartContents() {
-  // Get cart items
-  const cartItems = [getLocalStorage("so-cart")];
-  let objArr = new Array();
-
-  // Flatten levels because sometimes they nest
-  if (cartItems[0] != null) {
-    let items = cartItems[0].flat(10);
-    objArr = items.map((x) => JSON.parse(x));
-  }
-
-  // Deal with possible null entry
-  if (objArr[0] == null) {
-    objArr.shift();
-  }
-
+function getCartItemsFromStorage() {
   let dist = [];
+  const cartItems = [getLocalStorage("so-cart")];
+  let items = cartItems[0];
+  // Flatten 4 levels (why 4 well why not 4)
+  items = items.flat(4);
+  const objArr = items.map((x) => JSON.parse(x));
+
   // Loop through items to mark duplicates as additional value in qty
   for (let i in objArr) {
     // Qty count
@@ -44,14 +35,41 @@ function renderCartContents() {
     }
   }
 
+  return dist;
+}
+
+function renderCartContents() {
+  // Get Items
+  let dist = getCartItemsFromStorage();
+
   // Convert all object back to string
   const all = dist.map((x) => JSON.stringify(x));
 
   // Generate HTML from template for each item
   const htmlItems = all.map((i) => cartItemTemplate(i));
+  let total = 0;
+  for (let x in all) {
+    let item = JSON.parse(all[x]);
+    total += (Number(item["FinalPrice"]) * Number(item["qty"]));
+  }
+
+  // Hide no items in cart
+  let carttotal = ``;
+  if (dist.length > 0) {
+    carttotal = `<p class="cart-total">Total: $${total}</p>`;
+  }
+  else {
+    carttotal = `<p class="cart-total" hidden>Total: $${total}</p>`;
+  }
 
   // Assign HTML to page
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  document.querySelector(".cart-footer").innerHTML = carttotal;
+
+  var items = document.getElementsByClassName('remove-item');
+  for (var i = 0; i < items.length; i++) {
+    items[i].addEventListener('click', removeItemHandler);
+  }
 }
 
 function cartItemTemplate(item) {
@@ -62,6 +80,8 @@ function cartItemTemplate(item) {
   let color = obj["Colors"];
   let price = obj["FinalPrice"];
   let qty = obj["qty"];
+  let finalPrice = Number(price) * Number(qty);
+
   // Create html code
   const newItem = `<li class="cart-card divider">
   <a href="../product_pages/index.html?id=${obj["Id"]}" class="cart-card__image">
@@ -75,12 +95,35 @@ function cartItemTemplate(item) {
       <h2 class="card__name">${name}</h2>
     </a>
     <p class="cart-card__color">${color[0].ColorName}</p>
-    <p class="cart-card__quantity">qty: ${qty}</p>
-    <p class="cart-card__price">$${price}</p>
+    <p class="cart-card__quantity">Qty: ${qty}</p>
+    <p class="cart-card__price">$${finalPrice}</p>
   </div>
 </li>`;
 
   return newItem;
+}
+
+function removeItemFromCart(id) {
+  // Get Items
+  let dist = getCartItemsFromStorage();
+
+  // Convert all object back to string
+  const all = dist.map((x) => JSON.stringify(x));
+  let filteredArray = new Array;
+  for (let a in all) {
+    let item = JSON.parse(all[a]);
+    let itemId = item["Id"];
+    if (itemId != id) {
+      filteredArray.push(JSON.stringify(item));
+    }
+  }
+  setLocalStorage("so-cart", filteredArray);
+  renderCartContents();
+}
+
+function removeItemHandler(e) {
+  let id = e.target.dataset.id;
+  removeItemFromCart(id);
 }
 
 renderCartContents();

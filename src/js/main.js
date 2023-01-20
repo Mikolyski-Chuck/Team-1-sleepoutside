449757d9dd39acb55ae8fb0b84b5c154fe9a65a8
Brandon Smith <smi11037@byui.edu>
2023-01-19 18:47:46 -0700

    Team Project #2 Made Home and Product Details page dynamic.

---
+++ src/js/main.js
@@ -0,0 +1,54 @@
+import ProductData from "./ProductData.mjs";
+import { setCartSub } from "./cartsubscript.mjs";
+
+const dataSource = new ProductData("tents");
+
+async function renderIndex() {
+  // Get all tent data
+  var data = await dataSource.getAll();
+
+  // Top product Id's
+  var topIds = ["880RR", "985RF", "985PR", "344YJ"];
+
+  // Get item objects for item Id's that match Id's in topIds array
+  let topproduct = new Array();
+  for (let d in data) {
+    for (let t in topIds) {
+      if (data[d].Id == topIds[t]) {
+        topproduct.push(data[d]);
+      }
+    }
+  }
+  // Create html for top products
+  const htmlItems = topproduct.map((i) => itemTemplate(i));
+
+  // For all items use
+  //const htmlItems = topproduct.map((i) => itemTemplate(i));
+
+  // Combine all html into one variable and add it to the homepage
+  const finished = `<ul class="product-list">` + htmlItems.join("") + `</ul>`;
+  document.querySelector("#index-product-display").innerHTML = finished;
+}
+
+function itemTemplate(item) {
+  let name = item["Name"];
+  let img = item["Image"];
+  let brand = item["Brand"].Name;
+  let price = item["FinalPrice"];
+  const htmlItem = `
+  <li class="product-card">
+      <a href="product_pages/index.html?id=${item["Id"]}">
+        <img src="${img}" alt="${name}" />
+        <h3 class="card__brand">${brand}</h3>
+        <h2 class="card__name">${name}</h2>
+        <p class="product-card__price">$${price}</p>
+      </a>
+  </li>
+<br />
+`;
+
+  return htmlItem;
+}
+
+renderIndex();
+window.addEventListener("load", setCartSub);
--- src/js/product.js
+++ src/js/product.js
@@ -1,30 +1,5 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData("tents");
+const prodDetail = new ProductDetails("tents");
 
-function addProductToCart(product) {
-  // Get cart from storage
-  let cart = [getLocalStorage("so-cart")];
-
-  // Check if first product
-  if (cart.length == 1 && cart[0] == null) {
-    // If first set product in array
-    cart = [JSON.stringify(product)];
-  } else {
-    // If not the first product push string of product
-    cart.push(JSON.stringify(product));
-  }
-  // Set item "so-cart" in the local storage
-  setLocalStorage("so-cart", cart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+prodDetail.init();

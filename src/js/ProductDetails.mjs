9341be114d7eb9aed1a937d88f2c19da1fcabb0d
Chuck <mik17007@byui.edu>
2023-01-20 00:00:37 -0600

    re-add Brandon's code to eliminate nested arrays in local storage. add css styles for backpack subscript.

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,4 +1,5 @@
 import { getLocalStorage, setLocalStorage, getItemFromUrl} from "./utils.mjs";
+import { setCartSub } from "./cartsubscript.mjs";
 import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
@@ -66,8 +67,13 @@ export default class ProductDetails {
 
   async addProductToCart(product) {
     // Get cart from storage
-    let cart = [getLocalStorage("so-cart")];
-
+    //let cart = [getLocalStorage("so-cart")];
+    
+  /*
+    // Deal with possible null entry
+    if (objArr[0] == null) {
+      objArr.shift();
+    }
     // Check if first product
     if (cart.length == 1 && cart[0] == null) {
       // If first set product in array
@@ -76,10 +82,43 @@ export default class ProductDetails {
       // If not the first product push string of product
       cart.push(JSON.stringify(product));
     }
-
+    */
+  
+    // Parse current cart items if any and add to objArr
+    const cartItems = [getLocalStorage("so-cart")];
+    let objArr = new Array;
+    let newCart = new Array;
+  
+    // Parse current cart items if any and add to objArr
+    if (cartItems[0] != null) {
+      let items = cartItems[0].flat(10);
+      objArr = items.map((x) => JSON.parse(x));
+    }
+  
+    // Deal with possible null entry
+    if (objArr[0] == null) {
+      objArr.shift();
+    }
+  
+    // Add old items if any and add new item
+    if (objArr.length > 0) {
+      for (let x in objArr) {
+        newCart.push(JSON.stringify(objArr[x]));
+      }
+  
+      newCart.push(JSON.stringify(product));
+    }
+    // If no old items set new item as first item
+    else {
+      newCart = [JSON.stringify(product)];
+    }
+    
     // Set item "so-cart" in the local storage
-    setLocalStorage("so-cart", cart);
+    setLocalStorage("so-cart", newCart);
+    
+    //start Chuck Mikolyski
     setCartSub();
+    //End Chuck Mikolyski
   }
   async init(){
     let id = await getItemFromUrl();

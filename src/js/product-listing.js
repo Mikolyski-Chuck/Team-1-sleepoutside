8e4295628a9edaaef2ec30d5ec25b30a5bbdee56
Cricket999 <98436415+Cricket999@users.noreply.github.com>
2023-02-10 16:11:46 -0700

    Add sorting by name or price.

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -3,13 +3,16 @@ import ProductListing from "./ProductList.mjs";
 import { getItemFromUrl, loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
+initList();
 
-const category = getItemFromUrl("category");
-const newProductData = new ProductData();
-const listingElement = document.querySelector(".product-list");
-const newProductList = new ProductListing(
-  category,
-  newProductData,
-  listingElement
-);
-newProductList.init();
+export function initList() {
+  const category = getItemFromUrl("category");
+  const newProductData = new ProductData();
+  const listingElement = document.querySelector(".product-list");
+  const newProductList = new ProductListing(
+    category,
+    newProductData,
+    listingElement
+  );
+  newProductList.init();
+}

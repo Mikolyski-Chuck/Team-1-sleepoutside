f923d8fb6e72040fc7cb2e67629c5dc02e1306e3
minhahcf <u415635428@byui.edu>
2023-02-17 12:31:24 -0600

    update 2

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
-import Alert from "./alert.js"; 
+//import Alert from "./alert.js"; 
 
-const alert = new Alert();
-alert.displayAlert("alert-container");
+//const alert = new Alert();
+//alert.displayAlert("alert-container");
 const newProductData = new ProductData("tents");
 const listingElement = document.querySelector(".product-list");
 const newProductList = new ProductListing(
